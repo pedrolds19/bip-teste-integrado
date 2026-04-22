d7caf8f126a68a62b347f1be463c42cc503a61b0
Pedro Ferreira <111154668+pedrolds19@users.noreply.github.com>
2026-04-22 09:04:06 -0300

    Update Beneficio.java

--- ejb-module/src/main/java/com/example/domain/Beneficio.java
+++ ejb-module/src/main/java/com/example/domain/Beneficio.java
@@ -22,7 +22,6 @@ public class Beneficio implements Serializable {
     public Beneficio() {
     }
 
-    // --- Getters e Setters ---
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
